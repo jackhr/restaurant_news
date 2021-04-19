19308b28915493598b9f386f7849cd0a1f75dfa2
Stuart Klingman <stuartk@avatek.com>
2021-04-19 15:51:37 -0700

    placeholder files for html stuff

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('subscribe', views.subscribe, name='subscribe'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+  return HttpResponse("Hello, world. You're at the Restaurant News Subscriber Network.")
