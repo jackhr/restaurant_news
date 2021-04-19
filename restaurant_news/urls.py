aa5e96b2bc5666668e4f51d16d044c8305680c53
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-04-19 17:05:56 -0400

    change url root path to main_app

--- restaurant_news/urls.py
+++ restaurant_news/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-  path('polls/', include('polls.urls')),
+  path('', include('main_app.urls')),
   path('admin/', admin.site.urls),
 ]
