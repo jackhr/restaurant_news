e19477dde219b0eb605c92e94199783064a13076
Stuart Klingman <stuartk@avatek.com>
2021-04-19 15:58:02 -0700

    placeholder files for html stuff

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from .forms import SubscriberForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
-def index(request):
+def home(request):
   return HttpResponse("Hello, world. You're at the Restaurant News Subscriber Network.")
+
+class Subscriber(CreateView):
+  model = Subscriber
+  fields = "__all__"
+
+
