874b1b2edcc1d75ad8a9ffaafe37cf78790aa9de
Jack Huntington-Rainey <jack@Jacks-MacBook-Pro-2.local>
2021-04-20 14:24:42 -0400

    implement pie chart

--- main_app/views.py
+++ main_app/views.py
@@ -26,15 +26,34 @@ class SubscriberCreate(CreateView):
 
 def dashboard(request):
   subs = Subscriber.objects.all()
+  city_count = {
+    'seattle': 0,
+    'bellevue': 0,
+    'tacoma': 0,
+    'shoreline': 0,
+    'other': 0,
+  }
   for sub in subs:
     url = f"https://community-zippopotamus.p.rapidapi.com/us/{sub.zipcode}"
     response = requests.request("GET", url, headers=headers)
     results = response.json()
     place = results['places'][0]['place name']
     sub.placename = place
+    if sub.placename == 'Seattle':
+      city_count['seattle'] += 1
+    elif sub.placename == 'Bellevue':
+      city_count['bellevue'] += 1
+    elif sub.placename == 'Tacoma':
+      city_count['tacoma'] += 1
+    elif sub.placename == 'Shoreline':
+      city_count['shoreline'] += 1
+    else:
+      city_count['other'] += 1
+
   return render(request, 'dashboard.html', {
     'subs' : subs,
-    'citylookup' : CITYLOOKUP
+    'citylookup' : CITYLOOKUP,
+    'city_count': city_count,
   })
 
 
